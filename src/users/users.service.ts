fd673b39323c26a6dcdf6e7a4d658beca8c3262c
Moges741 <mogesse741@gmail.com>
2026-02-27 13:48:45 +0300

    for updating and delete

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -28,6 +28,32 @@ export class UsersService {
 
         return user;
     }
+    async update(id: number, dto: UpdateUserDto){
+        const user = await this.prisma.user.findUnique({
+            where: {id},
+        })
+         if(!user){
+            throw new NotFoundException('User Not Found!');
+        }
+    return this.prisma.user.update({
+      where: { id },
+      data: dto,
+    });
+    }
+    async remove(id: number) {
+
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
 
 
 }
