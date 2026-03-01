a0177d2d67718b68782e242b8b2677e57d04cb81
Moges741 <mogesse741@gmail.com>
2026-03-01 17:14:46 +0300

    added registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import {JwtService} from '@nestjs/jwt'
+import { RegisterDto } from '../users/dto/register.dto';
+import * as bcrypt from    'bcrypt';
 @Injectable()
 export class AuthService {
     constructor(
@@ -8,7 +10,18 @@ export class AuthService {
         private jwtService: JwtService
 
     ){}
-    
+    async register(dto: RegisterDto){
+        const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+        const user = this.prisma.user.create({
+            data: {
+                email: dto.email,
+                password: hashedPassword,
+            },
+        });
+        return "Created Successfully!"
+
+    }
 
 
 }
