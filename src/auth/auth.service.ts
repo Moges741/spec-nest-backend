1dc0760f6b968a1561a97ce4f64f64f0e6bfa2e7
Moges741 <mogesse741@gmail.com>
2026-03-01 17:08:30 +0300

    imported prisma and jwt services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from '../prisma/prisma.service';
+import {JwtService} from '@nestjs/jwt'
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private prisma: PrismaService,
+        private jwtService: JwtService
+
+    ){}
+    
+
+
+}
