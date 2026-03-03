0dd087e2c23a9763f533e370f8d663abbe88dbf1
Moges741 <mogesse741@gmail.com>
2026-03-03 14:14:06 +0300

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-import {ConfigModule} from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  // solve here?     
+  // solve here? 
+
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
   ],
-  imports: [UsersModule, PrismaModule, AuthModule],
+
+  // imports: [UsersModule, PrismaModule, AuthModule],
+ 
 })
 export class AppModule {}
